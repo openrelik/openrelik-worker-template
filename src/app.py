fd62ed4caf41e43565b74121131ec0b5c9468803
Johan Berggren <jberggren@gmail.com>
2024-09-22 09:09:43 +0200

    Add Redis client

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import os
 
+import redis
 from celery.app import Celery
 
 REDIS_URL = os.getenv("REDIS_URL")
 celery = Celery(broker=REDIS_URL, backend=REDIS_URL, include=["src.tasks"])
+redis_client = redis.Redis.from_url(REDIS_URL)
