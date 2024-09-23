c7149734073354d7decdc0d660914f7e930eeb17
Johan Berggren <jberggren@gmail.com>
2024-09-23 08:47:43 +0200

    Add License text and update dependency

--- src/tasks.py
+++ src/tasks.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import subprocess
 
 from openrelik_worker_common.utils import (
@@ -60,6 +74,7 @@ def command(
             output_path,
             filename=input_file.get("filename"),
             file_extension="<REPLACE_WITH_FILE_EXTENSION>",
+            data_type="<[OPTIONAL]_REPLACE_WITH_DATA_TYPE>",
         )
         command = base_command + [input_file.get("path")]
 
